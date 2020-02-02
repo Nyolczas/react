3fac7f1e7d42029e25c1a5063b6422018420d960
Nyolczas <nyolczas@gmail.com>
2020-02-02 16:34:10 +0100

    cards with names and images

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
-      string: 'Helló Bébi!'
-    }
+      monsters: []
+    };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({ string: 'Jó napot Kívánok!' })}>Üdvözülj!</button>
-        </header>
+      <CardList monsters={this.state.monsters}/>
       </div>
     );
   }
