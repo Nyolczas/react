9830e47a5602d8cf02cb2cc34e9a95a9ee93bf25
Nyolczas <nyolczas@gmail.com>
2020-02-03 13:55:06 +0100

    search-box component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -10,12 +11,19 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
   }
 
   render() {
@@ -26,10 +34,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input 
-          type="search" 
-          placeholder="keresés..." 
-          onChange={e=> this.setState({ searchField: e.target.value })}/>
+        <SearchBox 
+          placeholder='Keresés...'
+          handleChange={this.handleChange}
+        />
         <CardList monsters={filteredMonsters}/>
       </div>
     );
